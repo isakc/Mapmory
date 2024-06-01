fcf885807bcaaa2bff8cf069d287ed2a0de44d4b
lsakc15 <ckals15@naver.com>
2024-06-01 12:40:46 +0900

    Fix: 24.06.01: Search 수정
    필드 endRowNum, startRowNum 추가
    메소드 getEndRowNum(), getStartRowNum() 추가
    mybatis-config에 search typeAlias 추가

--- src/main/java/com/mapmory/common/domain/Search.java
+++ src/main/java/com/mapmory/common/domain/Search.java
@@ -19,4 +19,15 @@ public class Search {
 	private String userId;
 	private int categoryNo;
 	private int currentPage;
+	private int pageSize;
+	private int endRowNum;
+	private int startRowNum;	
+	
+	public int getEndRowNum() {
+		return getCurrentPage() * getPageSize();
+	}
+	
+	public int getStartRowNum() {
+		return (getCurrentPage()-1) * getPageSize()+1;
+	}
 }
