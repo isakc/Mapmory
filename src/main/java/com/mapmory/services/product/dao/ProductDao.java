f7e2e093130fb6c219eee85e234c08d9ab3f5d80
global <baramyeun@gmail.com>
2024-06-03 10:05:37 +0900

    FEAT :: 2024. 06. 03 :: PROUCT & PRODUCTIMAGE
    PRODUCT & PRODCUTIMAGE DAO SERVICE ADD COMM

--- src/main/java/com/mapmory/services/product/dao/ProductDao.java
+++ src/main/java/com/mapmory/services/product/dao/ProductDao.java
@@ -6,21 +6,24 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.mapmory.common.domain.Search;
 import com.mapmory.services.product.domain.Product;
+import com.mapmory.services.product.domain.ProductImage;
 
 @Mapper
 public interface ProductDao {
 
-	public void addProduct(Product product) throws Exception;
-	
-	public Product getDetailProduct(int productNo) throws Exception;
-	
-	public List<Product> getProductList(Search search) throws Exception;
-	
-	public void updateProduct(Product product) throws Exception;
-	
-	public void deleteProduct(int productNo) throws Exception;
-	
-	public Product getProductByName(String productTitle) throws Exception;
-	
-	public int getProductTotalCount(Search search) throws Exception;
+    public void addProduct(Product product) throws Exception;
+    
+    public Product getDetailProduct(int productNo) throws Exception;
+    
+    public List<Product> getProductList(Search search) throws Exception;
+    
+    public void updateProduct(Product product) throws Exception;
+    
+    public void deleteProduct(int productNo) throws Exception;
+    
+    public Product getProductByName(String productTitle) throws Exception;
+    
+    public int getProductTotalCount(Search search) throws Exception;
+
+    // 이미지 관련 메서드 추가
 }
