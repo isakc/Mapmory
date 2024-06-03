b327fb882970e330521722c37cfda7c757eb2fbc
globalstone <baramyeun@gmail.com>
2024-06-03 14:40:39 +0900

    COMMIT

--- src/test/java/com/mapmory/unit/product/ProductServiceTest.java
+++ src/test/java/com/mapmory/unit/product/ProductServiceTest.java
@@ -2,12 +2,16 @@ package com.mapmory.unit.product;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.mapmory.common.domain.Search;
 import com.mapmory.common.util.ImageFileUtil;
 import com.mapmory.services.product.domain.Product;
 import com.mapmory.services.product.domain.ProductImage;
@@ -19,7 +23,7 @@ public class ProductServiceTest {
     @Autowired
     ProductService productService;
 
-    @Test
+    //@Test
     public void testAddProduct() throws Exception {
         // Given
         Product product = new Product();
@@ -30,13 +34,77 @@ public class ProductServiceTest {
         product.setUserId("admin");
 
         List<String> imageFiles = new ArrayList<>();
-        imageFiles.add("test_image.jpg");
-        imageFiles.add("test_image11.jpg");
+        imageFiles.add("test_image12.jpg");
+        imageFiles.add("test_image13.jpg");
 
         // When
         productService.addProduct(product, imageFiles);
 
         // Then
-        // 추가로 확인해야 할 로직이 있다면 여기에 추가합니다.
+    }
+    
+    //@Test
+    public void testGetProductList() throws Exception {
+    	
+    	Search search = new Search();
+    	search.setCurrentPage(1);
+    	search.setPageSize(18);
+    	
+    	Map<String,Object> map = productService.getProductList(search);
+    	List<Product> list = (List<Product>)map.get("productList");
+    	
+    	System.out.println("리스트 확인 하세요 " +list);
+    	
+    	Assert.assertEquals(18, list.size());
+		
+		Integer totalCount = (Integer)map.get("productTotalCount");
+	 	System.out.println(totalCount);
+		
+	 	System.out.println("=======================================");
+	 	System.out.println("=======================================");
+    	
+    }
+    
+    //@Test
+    public void testUpdateProduct() throws Exception {
+        // Given
+        Product product = new Product();
+        product.setProductNo(17); // 업데이트할 제품 번호 설정
+        product.setProductTitle("Updated Test Product");
+        product.setPrice(15000);
+        product.setProductRegDate(LocalDateTime.now());
+        product.setPeriod(2);
+        product.setUserId("admin");
+
+        List<String> imageFiles = new ArrayList<>();
+        imageFiles.add("updated_test_image.jpg");
+        imageFiles.add("test_update_image.jpg");
+
+        // When
+        productService.updateProduct(product, imageFiles);
+
+    }
+    
+    //@Test
+    public void testDeleteImage() throws Exception {
+    	
+    	productService.deleteImage("c2e31b5692784d50ba294a81fb535b51.jpg");
+    	
+    	
+    }
+    
+    //@Test
+    public void testDeleteProduct() throws Exception {
+    	productService.deleteProduct(25);
+    }
+    
+    @Test
+    public void testGetDetailProduct() throws Exception {
+    	
+    	Product product = new Product();
+    	
+    	product = productService.getDetailProduct(26);
+    	
+    	System.out.println(product);
     }
 }
