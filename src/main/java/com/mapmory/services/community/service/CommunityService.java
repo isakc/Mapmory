45090f5ad15fd7b32e8a9b7831fe52a4bc135655
JaeYong93 <hyt9393@naver.com>
2024-06-03 21:33:52 +0900

    Feat :: 24. 06. 03 21:30 :: Community Service 구현
    Community Serivce 중 댓글 CRUD 구현

--- src/main/java/com/mapmory/services/community/service/CommunityService.java
+++ src/main/java/com/mapmory/services/community/service/CommunityService.java
@@ -15,7 +15,7 @@ public interface CommunityService {
 	public Map<String, Object> getReplyList(Search search, int recordNo) throws Exception;
 	
 	//내가 쓴 댓글 목록 조회
-	public List<Object> getUserReplyList(Search search, String userId) throws Exception;
+	public Map<String, Object> getUserReplyList(Search search, String userId) throws Exception;
 	
 	//댓글 작성
 	public void addReply(Reply reply) throws Exception;
@@ -27,16 +27,31 @@ public interface CommunityService {
 	public void updateReply(Reply reply) throws Exception;
 	
 	//댓글 삭제
-	public void deleteReply(Reply reply) throws Exception;
+	public void deleteReply(String userId, int replyNo) throws Exception;
 	
-	//기록 즐겨찾기 추가 및 해제
-	public void updateBookmarkSharedRecord(CommunityLogs communityLogs, String userId) throws Exception;
+	//기록 즐겨찾기 추가
+	public void addBookmarkSharedRecord(CommunityLogs communityLogs) throws Exception;	
 	
-	//기록 감정표현 추가, 수정, 삭제
-	public void updateReactionSharedRecord(CommunityLogs communityLogs, String userId) throws Exception;
+	//기록 즐겨찾기 해제
+	public void deleteBookmarkSharedRecord(String userId, int recordNo) throws Exception;
 	
-	//댓글 감정표현 추가, 수정, 삭제
-	public void updateReactionReply(CommunityLogs communityLogs, String userId) throws Exception;
+	// 기록 및 댓글에 대한 감정 표현 추가
+	public void addReaction(CommunityLogs communityLogs) throws Exception;    
+	    
+	// 기록 및 댓글에 대한 감정 표현 수정
+	public void updateReaction(CommunityLogs communityLogs) throws Exception;
+	
+	// 기록 및 댓글에 대한 감정 표현 삭제
+	public void deleteReaction(CommunityLogs communityLogs) throws Exception;	
+	
+//	// 댓글에 대한 감정 표현 추가
+//	public void addReactionReply(CommunityLogs communityLogs, String userId, int recordNo, int replyNo, int logsType) throws Exception;    
+//
+//	// 댓글에 대한 감정 표현 수정
+//	public void updateReactionReply(CommunityLogs communityLogs, String userId, int recordNo, int replyNo, int logsType) throws Exception;
+//
+//	// 댓글에 대한 감정 표현 삭제
+//	public void deleteReactionReply(CommunityLogs communityLogs, String userId, int recordNo, int replyNo, int logsType) throws Exception;
 	
 	//즐겨찾기 목록 조회
 	public Map<String, Object> getBookMarkSharedRecordList(Search search, String userId) throws Exception;
