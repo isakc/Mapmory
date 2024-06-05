7161ff8f9491cefac29ab1e0e894236ff9c27d74
globalstone <baramyeun@gmail.com>
2024-06-05 21:07:19 +0900

    Feat :: 24. 06. 05 :: Notice
    0. Notice Controller 테스트 완료.
    1. addNoticeOrFaq ( Add 할때는 드롭다운 메뉴로 공지사항 이냐 자주하는질문이냐 선택하여 등록 가능 if문 사용
    getNoticeList에서 공지 추가시 getNoticeList로 리다이렉트 getFaqList에서 공지 추가시
    getFaqList로 리다이렉트)
    2. getNoticeList( 완료 )
    3. getFaqList( 완료 )
    4. getDetailNotice ( NoticeNo로 판단)
    5. updateNotice ( 완료 )
    6. deleteNotice ( if 문 사용하여 삭제전 서비스에서 getDetailNotice를 가지고와서
    noticeType이0 이면 getNoticeList로 1이면 getFaqList로 보냄 )
    7. 불필요한 html 전부 삭제

--- src/main/java/com/mapmory/services/notice/dao/NoticeDao.java
+++ src/main/java/com/mapmory/services/notice/dao/NoticeDao.java
@@ -1,9 +1,9 @@
 package com.mapmory.services.notice.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
-
 import com.mapmory.common.domain.Search;
 import com.mapmory.services.notice.domain.Notice;
 
@@ -12,23 +12,16 @@ public interface NoticeDao {
  
 	public List<Notice> getNoticeList(Search search) throws Exception;
 	
+	public List<Notice> getFaqList(Search search) throws Exception;
+	
 	public Notice getDetailNotice(int noticeNo) throws Exception;
 	
 	public void updateNoticeAndFaq(Notice notice) throws Exception;
 	
-	public void addNotice(Notice notice) throws Exception;
+	public void addNoticeOrFaq(Notice notice) throws Exception;
 	
 	public void deleteNoticeAndFaq(int noticeNo) throws Exception;
 	
-	public List<Notice> getFaqList(Search search) throws Exception;
-	
-	public Notice getDetailFaq(int noticeNo) throws Exception;
-	
-	public void addFaq(Notice notice) throws Exception;
-	
 	public int getNoticeTotalCount(Search search) throws Exception;
-	
-	public int getFaqTotalCount(Search search) throws Exception;
-	
 	
 }
