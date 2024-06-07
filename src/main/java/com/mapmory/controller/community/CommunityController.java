818b54aad02244e3a333e669fb5c3a4fb5b7fd17
JaeYong93 <hyt9393@naver.com>
2024-06-07 17:16:28 +0900

    Fix :: 24.06.07 17:15 :: ContentFilterUtil 반영 안됨 수정
    ContentFilterUtil.java 파일 깃에 푸쉬되지 않았던 문제 발생하여 오류 수정함

--- src/main/java/com/mapmory/controller/community/CommunityController.java
+++ src/main/java/com/mapmory/controller/community/CommunityController.java
@@ -1,5 +1,45 @@
 package com.mapmory.controller.community;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.mapmory.services.community.domain.Reply;
+import com.mapmory.services.community.service.CommunityService;
+
+@Controller
+@RequestMapping("community/*")
 public class CommunityController {
 
+	@Autowired
+	@Qualifier("communityServiceImpl")
+	private CommunityService communityService;
+	
+	@Value("${pageUnit}")
+	int PageUnit;
+	
+	@Value("${pageSize}")
+	int PageSize;
+	
+	@PostMapping("/addReply")
+	public String addReply(MultipartHttpServletRequest request)  throws Exception{
+		
+		System.out.println("/community/addReply : POST 시작");
+		
+		MultipartFile file = request.getFile("replyImageName");
+		int recordNo = (Integer.parseInt(request.getParameter("recordNo")));
+		String userId = request.getParameter("userId");
+		String replyText = request.getParameter("replyText");
+		String replyImageName = null;
+	
+		
+		return null;
+	}
+	
+	
 }
