aa79b05f17f5446b2f3170efb45d21b4a3acb93f
globalstone <baramyeun@gmail.com>
2024-06-07 15:13:28 +0900

    Fix 24. 06. 07 15:12 :: ObjectStorageUtil
    import 수정

--- src/main/java/com/mapmory/common/util/ObjectStorageUtil.java
+++ src/main/java/com/mapmory/common/util/ObjectStorageUtil.java
@@ -12,7 +12,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.policy.Resource;
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
