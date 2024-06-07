6f43565731a6da6d4add58f345ce7409ad524ace
globalstone <baramyeun@gmail.com>
2024-06-07 17:55:18 +0900

    Feat 24. 06. 07 17:55 :: Clova Speech
    Test... Commit

---
+++ src/main/java/com/mapmory/controller/recommend/RecommendControllerJM.java
@@ -0,0 +1,30 @@
+package com.mapmory.controller.recommend;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.mapmory.common.util.ImageFileUtil;
+import com.mapmory.common.util.ObjectStorageUtil;
+
+public class RecommendControllerJM {
+
+	@Autowired
+	ObjectStorageUtil objectStorageUtil;
+	
+	@Value("${speech.folderName")
+	String speechFolderName;
+	
+	@PostMapping("/upload")
+	public void uploadAudio(@RequestParam("audio") MultipartFile audioFile) throws Exception {
+	    String originalFileName = audioFile.getOriginalFilename();
+	    String uuidFileName = ImageFileUtil.getProductImageUUIDFileName(originalFileName); // UUID 파일명 생성
+	    
+	    // ObjectStorageUtil을 사용하여 S3에 업로드
+	    objectStorageUtil.uploadFileToS3(audioFile, uuidFileName, speechFolderName);
+	}
+}
