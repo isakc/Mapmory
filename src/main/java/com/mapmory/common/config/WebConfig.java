b7cd3547c3c73551e245b3030f7bb904ab6a71ad
globalstone <baramyeun@gmail.com>
2024-06-10 13:21:57 +0900

    Fix :: 24. 06. 10 13:19 :: CORS
    CORS 오류 수정

--- src/main/java/com/mapmory/common/config/WebConfig.java
+++ src/main/java/com/mapmory/common/config/WebConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/video-call/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://192.168.0.22:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
