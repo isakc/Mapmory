70ccc680dfb6d42bde64aa19fde4234d00399e99
globalstone <baramyeun@gmail.com>
2024-06-10 13:41:16 +0900

    Fix :: 24. 06. 10 13:41 :: CORS
    CORS 수정

--- src/main/java/com/mapmory/controller/call/CallRestController.java
+++ src/main/java/com/mapmory/controller/call/CallRestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import com.mapmory.services.call.service.CallService;
 
 @RestController
 @RequestMapping("/call/*")
+@CrossOrigin(origins = "http://192.168.0.22:3000")
 public class CallRestController {
 
 	@Autowired
