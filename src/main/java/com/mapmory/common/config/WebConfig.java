cd343de001895f51ca7ebf99beacc8e19ef073c0
ooortcloud <110080440+ooortcloud@users.noreply.github.com>
2024-06-12 09:59:07 +0900

    Daily :: 24. 06. 12. 09:58 :: interceptor 추가 중
    현재 재민님 쪽에서 로그인이 수행되지 않는 이슈를 발견하여, 최신 버전을 업데이트 하기 위한 push

--- src/main/java/com/mapmory/common/config/WebConfig.java
+++ src/main/java/com/mapmory/common/config/WebConfig.java
@@ -2,8 +2,11 @@ package com.mapmory.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.mapmory.common.interceptor.LoginInterceptor;
 
 @Configuration
 @EnableWebMvc
@@ -13,4 +16,16 @@ public class WebConfig implements WebMvcConfigurer{
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
 	}
+
+	/*
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// TODO Auto-generated method stub
+		
+		registry.addInterceptor(new LoginInterceptor())
+				.order(1)
+				.addPathPatterns("/**")
+				.excludePathPatterns("/css/**", "/*.ico", "/error");
+	}
+	*/
 }
