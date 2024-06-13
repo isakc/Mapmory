d45e3a21e5f70d04348f6b61a96efb68afa97960
JaeYong93 <hyt9393@naver.com>
2024-06-13 17:01:02 +0900

    Feat :: 24.06.13. 17:00 :: 신고 기능 컨트롤러 완료
    신고기능 컨트롤러 완료, 기록 즐겨찾기 수정, 커뮤니티 로그 수정

--- src/main/java/com/mapmory/controller/community/CommunityController.java
+++ src/main/java/com/mapmory/controller/community/CommunityController.java
@@ -5,11 +5,13 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +21,7 @@ import com.mapmory.common.util.ImageFileUtil;
 import com.mapmory.common.util.ObjectStorageUtil;
 import com.mapmory.controller.timeline.TimelineController;
 import com.mapmory.services.community.domain.Reply;
+import com.mapmory.services.community.domain.Report;
 import com.mapmory.services.community.service.CommunityService;
 import com.mapmory.services.timeline.service.TimelineService;
 
@@ -85,6 +88,12 @@ public class CommunityController {
 	   
 		return "community/getReplyList";
     }	
+	
+	@GetMapping("/addReport")
+	public String addReport(Model model) throws Exception {
+		return "community/addReport";
+
+	}
 	
 	@PostMapping("/deleteReplyByRecord/{recordNo}")
 	public void deleteReplyByRecord(@PathVariable("recordNo") int recordNo) throws Exception {
