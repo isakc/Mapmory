3345d5436d366097adbdafca8c1ac8bee819e354
dlgudanr12 <dlgudanr12@naver.com>
2024-06-17 11:40:33 +0900

    Feat :: 24. 06. 17. 11:30 :: 성문 지원 및 hashtag 검색 완료
    성문 지원 및 hashtag 검색 완료

--- src/main/java/com/mapmory/controller/map/MapController.java
+++ src/main/java/com/mapmory/controller/map/MapController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mapmory.common.domain.Search;
 import com.mapmory.common.domain.SessionData;
 import com.mapmory.common.util.RedisUtil;
 
@@ -20,11 +22,12 @@ public class MapController {
 	
 	///// Method /////
 	@GetMapping(value="/map")
-	public String mapView(Model model, HttpServletRequest request) throws Exception {
-		
+	public String mapView(Model model, HttpServletRequest request,@RequestParam(name="searchKeyword",required = false) String searchKeyword) throws Exception {
 		//model.addAttribute("userId", redisUtil.getSession(request).getUserId());
 		model.addAttribute("userId", "user1");
-		
+		if(searchKeyword!=null) {
+			model.addAttribute("searchKeyword",searchKeyword);
+		}
 		return "map/map";
 	}// mapView
 }
