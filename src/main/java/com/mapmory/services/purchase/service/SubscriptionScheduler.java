974b09eac9e86534fe90f407cc186db45bbd51c2
lsakc15 <ckals15@naver.com>
2024-06-28 09:18:06 +0900

    Test: 2024.06.28 09:17: 구독 스케줄러 테스트

--- src/main/java/com/mapmory/services/purchase/service/SubscriptionScheduler.java
+++ src/main/java/com/mapmory/services/purchase/service/SubscriptionScheduler.java
@@ -1,5 +1,7 @@
 package com.mapmory.services.purchase.service;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -22,13 +24,13 @@ public class SubscriptionScheduler {
 	@Autowired
     private RedisLockUtil redisLockUtil;
 
-    private static final String LOCK_KEY = "subscriptionSchedulerLock";
-    private static final long LOCK_EXPIRE_TIME = 300000; // 5 minutes in milliseconds
+//    private static final String LOCK_KEY = "subscriptionSchedulerLock";
+//    private static final long LOCK_EXPIRE_TIME = 300000; // 5 minutes in milliseconds
 
 	@Scheduled(cron = "0 */5 * * * *") 
 	public void processSubscriptions() throws Exception {
-		if (redisLockUtil.acquireLock(LOCK_KEY, LOCK_EXPIRE_TIME)) {
-            try {
+//		if (redisLockUtil.acquireLock(LOCK_KEY, LOCK_EXPIRE_TIME)) {
+//            try {
                 List<Subscription> subscriptions = subscriptionService.getTodaySubscriptionList(); // 오늘 구독 결제일인 레코드 리스트
                 
                 for (Subscription subscription : subscriptions) {
@@ -41,15 +43,17 @@ public class SubscriptionScheduler {
                         e.printStackTrace(); //결제 실패 처리 로직
                     }//try~catch
                 }//for end
-            } finally {
-                redisLockUtil.releaseLock(LOCK_KEY);
-            }
-        }
+//            } finally {
+//                redisLockUtil.releaseLock(LOCK_KEY);
+//            }
+//        }
 	}//processSubscriptions: 매일 자정 결제일인 실행
 	
-	private Subscription updateSubscription(Subscription subscription) {
-		
-        subscription.setMerchantUid("subscription_" + subscription.getUserId() + "_" + LocalDateTime.now());
+	private Subscription updateSubscription(Subscription subscription) throws UnknownHostException {
+		InetAddress ip = InetAddress.getLocalHost();
+        String ipAddress = ip.getHostAddress();
+        
+        subscription.setMerchantUid("subscription_" + subscription.getUserId() + "_" + ipAddress);
         subscription.setSubscriptionStartDate(subscription.getSubscriptionEndDate());
         subscription.setSubscriptionEndDate(subscription.getSubscriptionEndDate().plusMinutes(5));
         subscription.setNextSubscriptionPaymentDate(subscription.getSubscriptionEndDate());
